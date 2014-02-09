eb95685e0b3c62d78667922c6a5693d138e17613
Anthony Short <antshort@gmail.com>
2014-02-08 16:50:36 -0800

    Compiler is working. Just need to create attribute and component bindings

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 var walk = require('dom-walk');
 var attributes = require('attributes');
 var emitter = require('emitter');
-var Observer = require('observer');
-var View = require('view');
+var find = require('find');
+var Component = require('component');
+var Attribute = require('attribute');
+
 
 /**
  * The compiler will take a set of views, an element and
  * a scope and process each node going down the tree. Whenever
  * it finds a node matching a directive it will process it.
- *
- * @param {Object} views Element bindings
- * @param {Object} bindings Attribute bindings
  */
-function Compiler(views, bindings) {
-  this.views = views || {};
-  this.bindings = bindings || {};
+function Compiler() {
+  this.components = [];
+  this.attributes = [];
 }
+
 
 /**
  * Mixin emitter
  */
 emitter(Compiler.prototype);
+
 
 /**
  * For plugins
@@ -34,172 +35,149 @@ Compiler.prototype.use = function(fn) {
   return this;
 };
 
+
+/**
+ * Add a component binding. This will be rendered as a separate
+ * view and have it's own scope.
+ *
+ * @param {String|Regex} matches String or regex to match an element name
+ * @param {Function} View
+ * @param {Object} options
+ */
+Compiler.prototype.addComponent = function(matches, View) {
+  this.components.push({
+    matches: matches,
+    view: View
+  });
+};
+
+
+/**
+ * Add an attribute binding. Whenever this attribute is matched
+ * in the DOM the function will be code with the current view
+ * and the element.
+ *
+ * @param {String|Regex} matches String or regex to match an attribute name
+ * @param {Function} process
+ * @param {Object} options
+ */
+Compiler.prototype.addAttribute = function(matches, process) {
+  this.attributes.push({
+    matches: matches,
+    process: process
+  });
+};
+
+
+/**
+ * Check if there's a component for this element
+ *
+ * @param {Element} el
+ *
+ * @return {Mixed}
+ */
+Compiler.prototype.getComponentBinding = function(el) {
+  return this.getBinding(el.nodeName.toLowerCase(), this.components);
+};
+
+
+/**
+ * Get the attribute binding for an attribute
+ *
+ * @param {String} attr
+ *
+ * @return {Mixed}
+ */
+Compiler.prototype.getAttributeBinding = function(attr) {
+  return this.getBinding(attr, this.attributes);
+};
+
+
+/**
+ * Get the attribute binding for an attribute
+ *
+ * @param {String} attr
+ *
+ * @return {Mixed}
+ */
+Compiler.prototype.getBinding = function(name, bindings) {
+  return find(bindings, function(binding){
+    if(name === binding.matches) {
+      return binding;
+    }
+    if(binding.matches.test(name)) {
+      return binding;
+    }
+  });
+};
+
+
 /**
  * Compile a node with a given scope, traversing down
  * the tree and applying all the views.
  *
- * @param {Element} node
- * @param {Scope} scope
- *
- * @api public
- * @return {Element}
- */
-Compiler.prototype.compile = function(node, scope){
-  this.scope = scope;
-  this.view = new View(node, scope);
-  walk(node, this.compileNode.bind(this));
-  return this.view;
-};
-
-/**
- * Compile a single node
- *
- * @param {Scope} scope
- * @param {Element} el
- * @param {Function} next
- *
- * @api private
- * @return {void}
- */
-Compiler.prototype.compileNode = function(el, next) {
-  // Text nodes don't do anything, but this gives plugins
-  // a chance to hook in an do something.
-  if(el.nodeType === 3) {
-    this.emit('text', this.scope, el);
-    return next();
-  }
-
-  this.emit('node', this.scope, el, next);
-
-  // Check to see if this element is a custom view
-  var view = this.getView(el);
-
-  // This node is a custom view
-  if(view) {
-    this.compileViews(el, view);
-    return next();
-  }
-
-  // It's a normal element, so just process the bindings
-  this.compileBindings(el, next);
-};
-
-/**
- * Get the directive for an element if it exists
- *
- * @param {Element} el
- *
- * @return {Function} Returns undefined if it isn't a directive
- */
-Compiler.prototype.getView = function(el) {
-  return this.views[el.nodeName.toLowerCase()];
-};
-
-/**
- * Compile the directive for this element if it exists.
- *
- * @param {Observer} scope
- * @param {Element} el
- * @param {Function} next
- *
- * @return {void}
- */
-Compiler.prototype.compileViews = function(el, View, next) {
-  var scope = this.scope;
-  var props = this.getProperties(el);
-  var view = new View(el, state);
-  this.bindProperties(view, scope, props);
-  this.bindEvents(view, scope, el);
-  this.view.children.push(view);
-  this.emit('view', el, view, props, this);
-};
-
-/**
- * Get the properties from an element to pass to the
- * child directive.
- *
- * @param {Element} el
+ * @param {View} view
  *
  * @return {Object}
  */
-Compiler.prototype.getProperties = function(el) {
-  return el.dataset;
-};
+Compiler.prototype.compile = function(view){
 
-/**
- * Bind properties from one scope to another
- *
- * @param {Observer} scope
- * @param {Observer} properties
- * @param {Object} props
- *
- * @return {void}
- */
-Compiler.prototype.bindProperties = function(view, scope, props){
-  Object.keys(props).forEach(function(name){
-    var scopeKey = props[name];
-    scope.change(scopeKey, function(val){
-      view.set(name, val);
-    })
-  });
-};
+  // Attach the view to a fragment for speedy parsing
+  var fragment = document.createDocumentFragment();
+  fragment.appendChild(view.el);
 
-/**
- * Bind events from a child directive to the parent
- * view. Events are emitted from the child directive
- *
- * @param {Object} view
- * @param {Observer} scope
- * @param {Element} el
- *
- * @return {void}
- */
-Compiler.prototype.bindEvents = function(view, el) {
-  if(!view.emit) return; // Not an emitter
-  var self = this;
-  var scope = this.scope;
-  var events = el.getAttribute('events');
-  var fn = new Function("return " + events);
-  var mapping = fn.call(scope);
-  Object.keys(mapping).forEach(function(name){
-    var callback = mapping[name];
-    view.on(name, callback.bind(self));
-  });
-};
+  // Store references to the bindings so
+  // that we can dispose of them later
+  var attributeBindings = [];
+  var componentBindings = [];
 
-/**
- * Compile bindings on elements and bind them
- * to the current scope
- *
- * @param {Observer} scope
- * @param {Element} el
- *
- * @return {void}
- */
-Compiler.prototype.compileBindings = function(el, next) {
-  var scope = this.scope;
-  var attrs = attributes(el);
-  for(var attr in attrs) {
-    var value = attrs[attr];
-    var obj = this.bindings[attr];
-    if(!obj) continue;
-    var process = obj.process;
-    var options = obj.options;
-    var attributeValue = el.getAttribute(obj.name);
-    var binding = process.call(this, el, attr, value);
+  walk(view.el, function(node, next){
 
-    if(binding.skip === true) {
-      // skip children
+    // Render text nodes. Interpolate the text node using the
+    // views interpolate method. Whenever the view model changes
+    // it will re-render the text node.
+    if(node.nodeType === 3) {
+      view.interpolate(node.data, function(val){
+        node.data = val;
+      });
+      return next();
     }
 
-    if(!binding.unbind) {
-      throw new Error('Binding');
+    // Render components. We'll create a view for this component
+    // and bind to any data or events. Then we'll replace the
+    // placeholder node in the template with the rendered view
+    var component = this.getComponentBinding(node);
+    if(component) {
+      componentBindings.push(new Component(view, node, component));
+      return next();
     }
-    // Options might 1. prevent other bindings, 2. skip processing children
-    this.view.bindings.push(binding);
-    this.emit('binding', scope, el, binding, obj);
-  }
+
+    // Render attributes. If any of the attributes on the node
+    // match one of our bindings we'll run the binding on the node.
+    // Otherwise we'll interpolate the attribute which will update
+    // whenever the view model changes.
+    var attrs = attributes(node);
+
+    Object.keys(attrs).forEach(function(attr){
+      var binding = this.getAttributeBinding(attr);
+      if(!binding) {
+        view.interpolate(attrs[attr], function(val){
+          node.setAttribute(attr, val);
+        });
+        return next();
+      }
+      attributeBindings.push(new Attribute(this, view, node, attr));
+    }, this);
+
+    next();
+  }.bind(this));
+
+  // trigger all bindings
+  view.emit('bind');
+
+  return view;
 };
+
 
 /**
  * Exports
--- test/index.js
+++ test/index.js
@@ -1,75 +1,91 @@
 var Compiler = require('compiler');
 var assert = require('assert');
 var emitter = require('emitter');
+var createView = require('view');
 
 describe('compiler', function(){
+  var compiler;
 
-  it('should be created', function(done){
-    try {
-      var compiler = new Compiler();
+  beforeEach(function(){
+    compiler = new Compiler();
+  });
+
+  it('should compile a view', function(){
+    var compiler = new Compiler();
+    var View = createView('<div></div>');
+    var view = new View();
+    compiler.compile(view);
+  })
+
+  it('should match attributes with a string', function(done){
+    var compiler = new Compiler();
+    compiler.addAttribute('data-test', function(view, el, attr, value){
+      assert(value === "foo");
+      done();
+    });
+    var View = createView('<div data-test="foo"></div>');
+    var view = new View();
+    compiler.compile(view);
+  });
+
+  it('should match attributes with a regex', function(done){
+    var compiler = new Compiler();
+    compiler.addAttribute(/test/, function(view, el, attr, value){
+      assert(value === "foo");
+      done();
+    });
+    var View = createView('<div data-test="foo"></div>');
+    var view = new View();
+    compiler.compile(view);
+  });
+
+  it('should match components with a string', function(done){
+    var compiler = new Compiler();
+    function Dummy(){
       done();
     }
-    catch(e) {
-      done(false);
-    }
+    compiler.addComponent('dummy', Dummy);
+    var View = createView('<dummy></dummy>');
+    var view = new View();
+    compiler.compile(view);
+  });
+
+  it('should interpolate text nodes', function(){
+    var View = createView('<div>{{foo}}</div>');
+    var view = new View({
+      foo: 'bar'
+    });
+    compiler.compile(view);
+    assert(view.el.innerHTML === 'bar');
   })
 
-  it('should have directives', function(){
-    var compiler = new Compiler({ foo: 'bar' });
-    assert(compiler.views.foo === 'bar');
+  it('should update interpolated text nodes', function(){
+    var View = createView('<div>{{foo}}</div>');
+    var view = new View({
+      foo: 'bar'
+    });
+    compiler.compile(view);
+    view.set('foo', 'baz');
+    assert(view.el.innerHTML === 'baz');
   })
 
-  it('should be an emitter', function(){
-    var compiler = new Compiler();
-    assert(compiler.on);
-    assert(compiler.off);
-    assert(compiler.emit);
+  it('should interpolate attributes', function(){
+    var View = createView('<div id="{{foo}}"></div>');
+    var view = new View({
+      foo: 'bar'
+    });
+    compiler.compile(view);
+    assert(view.el.id === 'bar');
   })
 
-  it('should compile a node', function(){
-    var el = document.createElement('div');
-    var compiler = new Compiler();
-    var view = compiler.compile(el);
-  })
-
-  it('should emit events on nodes', function(done){
-    var el = document.createElement('div');
-    var compiler = new Compiler();
-    compiler.on('node', function(scope, element){
-      assert(element === el);
-      done();
-    })
-    compiler.compile(el);
-  })
-
-  describe('Views', function(){
-
-    it('should compile views', function(done){
-      var compiler = new Compiler({
-        'commentbox': function(){
-          done();
-        }
-      });
-      var el = document.createElement('CommentBox');
-      compiler.compile(el);
-    })
-
-    it('should bind to events if view is an emitter', function(done){
-      function CommentBox() {}
-      emitter(CommentBox.prototype);
-      var compiler = new Compiler({
-        'commentbox': CommentBox
-      });
-      var el = document.createElement('CommentBox');
-      el.setAttribute('events', '{ add: this.get("handleAdd") }');
-      var view = compiler.compile(el, {
-        handleAdd: function(){
-          done();
-        }
-      });
-      view.children[0].emit('add');
-    })
-
+  it('should update interpolated attributes', function(){
+    var View = createView('<div id="{{foo}}"></div>');
+    var view = new View({
+      foo: 'bar'
+    });
+    compiler.compile(view);
+    view.set('foo', 'baz');
+    assert(view.el.id === 'baz');
   })
 
 
